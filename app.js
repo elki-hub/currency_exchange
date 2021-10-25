8574a182db5f1100cace196603a527f7c076b84b
Elze <elze.kilaite@mif.stud.vu.lt>
2021-10-25 09:53:21 +0300

    responding with json object

--- app.js
+++ app.js
@@ -2,39 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
-//const fetch = require("node-fetch");
 const axios = require('axios');
-//import fetch from 'node-fetch';
-//const fetch = require('fetch');
-
-
 
 const rateURL = 'https://api.exchangerate-api.com/v4/latest/'
 
-// axios.get(rateURL)
-//     .then(function (response){
-//     console.log(response.data);
-// })
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-//app.set("view engine", "ejs");
-//app.use(express.static(__dirname + '/public'));
-//app.set('views', path.join(__dirname, 'views'));
-
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 
 app.get('/', (req, res) =>{
-
-
-    res.render('index', {
-        data: ''
-    })
 })
-
-
-
 
 app.post('/quote', async (req, res) =>{
     let {baseCurrency, quoteCurrency, baseAmount} = req.body;
@@ -49,27 +25,11 @@ app.post('/quote', async (req, res) =>{
         quoteAmount: quoteAmount
     }
 
-    console.log(typeof exchange)
-    console.log(exchange)
+    //console.log(typeof exchange)
+    //console.log(exchange)
 
+    res.json(exchange)
 
-    let json = JSON.stringify(exchange);
-    console.log(typeof json)
-
-
-    res.render('index', {
-        data:{
-            baseCurrency: baseCurrency,
-            quoteCurrency: quoteCurrency,
-            baseAmount: baseAmount
-        },
-        //exchange: exchange
-    })
-
-})
-
-app.post('/', (req, res) =>{
-    res.send('Got a POST request');
 })
 
 app.listen(port, ()=>{
